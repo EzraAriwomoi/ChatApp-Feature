79b13a0984f94468915fb34c8d377c2576c63485
Ezra Ariwomoi <kropezra@gmail.com>
2024-08-16 12:34:25 +0300

    last seen

--- lib/common/helper/last_seen_message.dart
+++ lib/common/helper/last_seen_message.dart
@@ -1,16 +1,42 @@
-String lastSeenMessage(lastSeen) {
+String lastSeenMessage(int lastSeen) {
   DateTime now = DateTime.now();
-  Duration differenceDuration = now.difference(
-    DateTime.fromMillisecondsSinceEpoch(lastSeen),
-  );
+  DateTime lastSeenDateTime = DateTime.fromMillisecondsSinceEpoch(lastSeen);
+  Duration differenceDuration = now.difference(lastSeenDateTime);
 
-  String finalMessage = differenceDuration.inSeconds > 59
-      ? differenceDuration.inMinutes > 59
-          ? differenceDuration.inHours > 23
-              ? "${differenceDuration.inDays} ${differenceDuration.inDays == 1 ? 'day' : 'days'}"
-              : "${differenceDuration.inHours} ${differenceDuration.inHours == 1 ? 'hour' : 'hours'}"
-          : "${differenceDuration.inMinutes} ${differenceDuration.inMinutes == 1 ? 'minute' : 'minutes'}"
-      : 'few moments';
+  if (differenceDuration.inMinutes < 1) {
+    return 'Last seen just now';
+  } else if (differenceDuration.inMinutes < 60) {
+    return 'Last seen ${differenceDuration.inMinutes} ${differenceDuration.inMinutes == 1 ? 'minute' : 'minutes'} ago';
+  } else if (differenceDuration.inHours < 24) {
+    return 'Last seen ${differenceDuration.inHours} ${differenceDuration.inHours == 1 ? 'hour' : 'hours'} ago';
+  } else if (differenceDuration.inDays == 1) {
+    return 'Last seen yesterday at ${_formatTime(lastSeenDateTime)}';
+  } else if (differenceDuration.inDays < 7) {
+    String dayOfWeek = _formatDayOfWeek(lastSeenDateTime.weekday);
+    return 'Last seen on $dayOfWeek at ${_formatTime(lastSeenDateTime)}';
+  } else {
+    return 'Last seen on ${_formatDate(lastSeenDateTime)} at ${_formatTime(lastSeenDateTime)}';
+  }
+}
 
-  return finalMessage;
+String _formatTime(DateTime dateTime) {
+  // Formats time as '10:13 AM' or '10:13 PM'
+  return "${dateTime.hour > 12 ? dateTime.hour - 12 : dateTime.hour}:${dateTime.minute.toString().padLeft(2, '0')} ${dateTime.hour >= 12 ? 'PM' : 'AM'}";
+}
+
+String _formatDayOfWeek(int weekday) {
+  // Returns the day of the week from an integer
+  List<String> days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  return days[weekday - 1];
+}
+
+String _formatDate(DateTime dateTime) {
+  // Formats the date as 'August 12, 2024'
+  return "${_formatMonth(dateTime.month)} ${dateTime.day}, ${dateTime.year}";
+}
+
+String _formatMonth(int month) {
+  // Returns the month name from an integer
+  List<String> months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return months[month - 1];
 }
