00f2e4f25041000d6d492c0cb705a817f6b898ae
Ariwomoi <128458668+EzraAriwomoi@users.noreply.github.com>
2024-09-14 13:23:59 +0300

    yellow card

--- lib/features/chat/widgets/yellow_card.dart
+++ lib/features/chat/widgets/yellow_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ult_whatsapp/common/extension/custom_theme_extension.dart';
+import '../../popups/end_to_end_encrypted.dart';
 
 class YellowCard extends StatelessWidget {
   const YellowCard({
@@ -8,22 +9,43 @@ class YellowCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(
-        vertical: 10,
-        horizontal: 30,
-      ),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: context.theme.yellowCardBgColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Text(
-        'Message and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them. Tap to learn more.',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 13,
-          color: context.theme.yellowCardTextColor,
+    return GestureDetector(
+      onTap: () {
+        showEndToEndBottomSheet(context);
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 30,
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 20),
+        decoration: BoxDecoration(
+          color: context.theme.yellowCardBgColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            children: [
+              WidgetSpan(
+                child: Icon(
+                  Icons.lock_outline_rounded,
+                  size: 10,
+                  color: context.theme.yellowCardTextColor,
+                ),
+                alignment: PlaceholderAlignment.middle,
+              ),
+              TextSpan(
+                text:
+                    ' Messages and calls are end-to-end encrypted. No one outside of this\nchat, not even WhatsApp, can read or listen to them. Tap to learn more.',
+                style: TextStyle(
+                  fontSize: 11,
+                  fontFamily: 'Arial',
+                  color: context.theme.yellowCardTextColor,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
