5f891269ad317c52095220d3703b946e5ff385a6
Ariwomoi <128458668+EzraAriwomoi@users.noreply.github.com>
2024-10-06 23:30:19 +0300

    url

--- lib/features/widget/privacy_and_terms.dart
+++ lib/features/widget/privacy_and_terms.dart
@@ -1,8 +1,30 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:ult_whatsapp/common/extension/custom_theme_extension.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PrivacyAndTerms extends StatelessWidget {
   const PrivacyAndTerms({super.key});
+
+  Future<void> _launchPrivacyPolicyURL() async {
+    const privacyUrl =
+        'https://www.whatsapp.com/legal/privacy-policy?lg=en&lc=US';
+    if (await canLaunchUrl(Uri.parse(privacyUrl))) {
+      await launchUrl(Uri.parse(privacyUrl));
+    } else {
+      throw 'Could not launch $privacyUrl';
+    }
+  }
+
+  Future<void> _launchTermsOfServiceURL() async {
+    const termsUrl =
+        'https://www.whatsapp.com/legal/terms-of-service?lg=en&lc=US';
+    if (await canLaunchUrl(Uri.parse(termsUrl))) {
+      await launchUrl(Uri.parse(termsUrl));
+    } else {
+      throw 'Could not launch $termsUrl';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +35,10 @@ class PrivacyAndTerms extends StatelessWidget {
         text: TextSpan(
             text: 'Read our ',
             style: TextStyle(
-              color: context.theme.greyColor,
-              fontSize: 12,
-              letterSpacing: 0,
-              height: 1.5
-            ),
+                color: context.theme.greyColor,
+                fontSize: 12,
+                letterSpacing: 0,
+                height: 1.5),
             children: [
               TextSpan(
                 text: 'Privacy Policy. ',
@@ -25,6 +46,8 @@ class PrivacyAndTerms extends StatelessWidget {
                   color: context.theme.blueColor,
                   letterSpacing: 0,
                 ),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = _launchPrivacyPolicyURL,
               ),
               const TextSpan(text: 'Tap "Agree and continue" to accept the '),
               TextSpan(
@@ -33,6 +56,8 @@ class PrivacyAndTerms extends StatelessWidget {
                   color: context.theme.blueColor,
                   letterSpacing: 0,
                 ),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = _launchTermsOfServiceURL,
               ),
             ]),
       ),
